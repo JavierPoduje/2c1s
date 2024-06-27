8ca66f5bc7ad1f052085c3bc43cd336e1c363eb3
javierpoduje <javier.poduje@gmail.com>
2024-06-26 20:40:29 -0400

    lazygit baby, yeah....

--- server/conways/game.go
+++ server/conways/game.go
@@ -12,13 +12,39 @@ func NewGame() *Game {
 
 func newBoard() [][]byte {
 	board := [][]byte{}
+	seed := seedCoords()
 
-	for i := 0; i < BoardWidth; i++ {
+	for y := 0; y < BoardWidth; y++ {
 		row := []byte{}
-		for j := 0; j < BoardHeight; j++ {
-			row = append(row, byte(0))
+		for x := 0; x < BoardHeight; x++ {
+			var currSeed []int
+			if len(seed) > 0 {
+				currSeed = seed[0]
+			}
+
+			if len(currSeed) > 0 && currSeed[0] == y && currSeed[1] == x {
+				row = append(row, byte(1))
+				seed = seed[1:]
+			} else {
+				row = append(row, byte(0))
+			}
+
 		}
 		board = append(board, row)
 	}
 	return board
+}
+
+func seedCoords() [][]int {
+	return [][]int{
+		{4, 4},
+		{4, 5},
+		{5, 3},
+		{5, 6},
+		{6, 3},
+		{6, 6},
+		{7, 3},
+		{7, 4},
+		{7, 5},
+	}
 }
