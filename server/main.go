610b66828e3e5d16acc02da587ecc6244a52df58
javierpoduje <javier.poduje@gmail.com>
2024-07-12 20:12:52 -0400

    feat: add a few more seeds

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/javierpoduje/2c1s/server/sender"
 	"github.com/joho/godotenv"
+	"math/rand"
 )
 
 const (
@@ -12,12 +13,7 @@ const (
 	BoardHeight = 13
 )
 
-// place the diamong right at the top-left of the board
-func diamondRing(height, width int) [][]int {
-	if width < 13 || height < 13 {
-		log.Panic("Board must be at least 13x13 to display the diamond-ring")
-	}
-
+func diamondRing() [][]int {
 	return [][]int{
 		{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
@@ -35,14 +31,61 @@ func diamondRing(height, width int) [][]int {
 	}
 }
 
+func achimsP4() [][]int {
+	return [][]int{
+		{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+		{1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0},
+		{1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+}
+
+func fourEightTwelveDiamond() [][]int {
+	return [][]int{
+		{0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+}
+
 func main() {
 	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	seed := diamondRing(BoardHeight, BoardWidth)
+	seeds := []func() [][]int{
+		diamondRing,
+		achimsP4,
+		fourEightTwelveDiamons,
+	}
 
-	s := sender.NewServer(BoardWidth, BoardHeight, seed)
-	s.Start(BoardWidth, BoardHeight, seed)
+	// make a random choose between the seeds, copilot
+	seed := seeds[randomIdx(seeds)]
+
+	s := sender.NewServer(BoardWidth, BoardHeight, seed())
+	s.Start(BoardWidth, BoardHeight, seed())
+}
+
+func randomIdx(seeds []func() [][]int) int {
+	return rand.Int() % len(seeds)
 }
